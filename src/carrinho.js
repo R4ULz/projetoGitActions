9c359a632889fd8a642baa7a7c78cb3ca9cb4246
RAUL DOS SANTOS ARAUJO <122099251+R4ULz@users.noreply.github.com>
2025-06-04 08:43:48 -0300

    calcularTotal funcionando

--- src/carrinho.js
+++ src/carrinho.js
@@ -6,14 +6,27 @@ class Carrinho{
     }
 
     adicionarItem(item){
-        this.itens.push(item)
+        if(!item || !item.nome || !item.preco) {
+            throw new Error('Digite um item válido');
+        }else{
+            this.itens.push(item);
+        }
     }
 
     listarItens() {
-        return this.itens;
+        if(!this.itens.length) {
+            throw new Error('Carrinho vazio');
+        }
+        return this.itens;  
+        
     }
 
     calcularTotal(){
+        if(!this.itens.length) {
+            throw new Error('Carrinho vazio');
+        }
+        this.total = this.itens.reduce((acc, item) => acc + item.preco, 0);
+        return this.total;
     }
 
     removerItem(nome){
